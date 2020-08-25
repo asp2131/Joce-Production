4ff358f343624cf4594a9a04ad8dfe9171dbeadf
asp2131 <akinpds@gmail.com>
2020-08-25 12:07:24 -0500

    Rendered logo onto landing screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, NormalizedCacheObject  } from '@apollo/client';
+
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   uri: 'http://localhost:4000/',
@@ -27,6 +29,7 @@ export default function App() {
   return (
     // <ApolloProvider client={client} >
       <View style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/text-logo.png')} />
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
       </View>
@@ -35,10 +38,16 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {   
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8e44ad',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 200,
+    height: 200,
+    bottom: 200,
+    resizeMode: 'contain'
+  }
 });
