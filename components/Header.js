85e3c7bfe2862b0819e263f19c419389679fb868
asp2131 <akinpds@gmail.com>
2020-09-03 17:32:35 -0500

    Rendered Explore view header

---
+++ components/Header.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet } from 'react-native'
+import { Container, Header, Tab, Tabs, ScrollableTab, Icon, Button, Body, Right } from 'native-base';
+
+export default function TabsScrollableExample(){
+    
+        return (
+            <Container  >
+                <Header androidStatusBarColor="#e6deff" iosBarStyle="#e6deff"  hasTabs transparent>
+                    <Body>
+                        <Text style={styles.header}>Explore</Text>
+                    </Body>
+                    <Right>
+                        <Button
+                        style={styles.nav}
+                        >
+                            <Icon style={{color: "black"}} name='search' />
+                        </Button>
+                    </Right>
+                </Header>
+                <Tabs renderTabBar={() => <ScrollableTab />}>
+                    <Tab
+                        activeTabStyle={{ backgroundColor: 'white' }}
+                        activeTextStyle={{ color: "black" }}
+                        tabStyle={{ backgroundColor: 'white' }}
+                        heading="Fashion">
+                        <Text >Tab 1</Text>
+                    </Tab>
+                    <Tab
+                        heading="Sports"
+                        activeTabStyle={{ backgroundColor: 'white' }}
+                        activeTextStyle={{ color: "black" }}
+                        tabStyle={{ backgroundColor: 'white' }}>
+                        <Text>Tab 2</Text>
+                    </Tab>
+                    <Tab
+                        heading="Tech"
+                        activeTabStyle={{ backgroundColor: 'white' }}
+                        activeTextStyle={{ color: "black" }}
+                        tabStyle={{ backgroundColor: 'white' }}>
+                        <Text>Tab 3</Text>
+                    </Tab>
+                    <Tab
+                        heading="Music"
+                        activeTabStyle={{ backgroundColor: 'white' }}
+                        activeTextStyle={{ color: "black" }}
+                        tabStyle={{ backgroundColor: 'white' }}>
+                        <Text>Tab 4</Text>
+                    </Tab>
+                    <Tab
+                        heading="Gaming"
+                        activeTabStyle={{ backgroundColor: 'white' }}
+                        activeTextStyle={{ color: "black" }}
+                        tabStyle={{ backgroundColor: 'white' }}>
+                        <Text>Tab 5</Text>
+                    </Tab>
+                </Tabs>
+            </Container>
+        );
+    
+}
+
+
+const styles = StyleSheet.create({
+    text:{
+        color: 'black'
+    },
+    header: {
+        fontWeight: "bold",
+        fontSize: 32
+    },
+    nav: {
+        backgroundColor: '#ffff'
+    }
+})
